d037c134cbe0636bd7b048428a03b5e9dff33ceb
muskann12 <nisarmuskan14@gmail.com>
2024-04-21 15:08:06 -0700

    number-guessing game

---
+++ index.ts
@@ -0,0 +1,29 @@
+#!usr bin/env node
+import inquirer from "inquirer";
+
+console.log("heylo im muskan nisar");
+
+const randomnumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+       name: "userGuessNumber" ,
+       type: "number",
+       message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+]);
+
+if(answer.userGuessNumber === randomnumber){
+    console.log( " congrats! you guessed a crorrect number");
+
+
+}
+else {
+    console.log("sorry you guessed a wrong number");
+}
+
+
+
+
+
+
